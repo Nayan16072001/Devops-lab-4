5eb15def391ae2df99b9706579994ed3821b1870
Nayan Gupta <2021wa86688@wilp.bits-pilani.ac.in>
2025-07-12 16:26:17 +0530

    Updated App.java locally

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -2,7 +2,7 @@ package com.devops.lab;
 
 public class App
 {
-    public static void main ( string [] args )
+    public static void main ( String [] args )
     {
                System.out.println("Hello From Continous Integration Trigger!");
                System.out.println("Devops Lab 4 completes if the first Line of Output shows Started by SCM");
